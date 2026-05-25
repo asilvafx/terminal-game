8fbd9264c3cbf64601c9f308e3c86c9ea15de3d4
António Da Silva <73810823+asilvafx@users.noreply.github.com>
2026-05-25 14:21:29 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ sudo apt install -y python3 python3-pip python3-venv git python3-full
 
 cd terminal-game
 
-# create venv if not exists
+# create venv
 if [ ! -d "venv" ]; then
     python3 -m venv venv
 fi
@@ -18,5 +18,32 @@ source venv/bin/activate
 
 pip install -r requirements.txt
 
-echo "DONE. Run with:"
-echo "source venv/bin/activate && python game.py"
+echo "Setting up system service..."
+
+sudo tee /etc/systemd/system/terminal-game.service > /dev/null <<EOF
+[Unit]
+Description=Terminal Horror Game
+After=plymouth-quit.service systemd-user-sessions.service
+Wants=plymouth-quit.service
+
+[Service]
+Type=simple
+User=pi
+WorkingDirectory=/home/pi/terminal-game
+
+ExecStartPre=/home/pi/terminal-game/venv/bin/python /home/pi/terminal-game/boot_loader.py
+ExecStart=/home/pi/terminal-game/venv/bin/python /home/pi/terminal-game/game.py
+
+Restart=always
+StandardInput=tty
+TTYPath=/dev/tty1
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reexec
+sudo systemctl enable terminal-game.service
+
+echo "INSTALL COMPLETE."
+echo "Reboot to start Terminal."
